e5097b9856c4bbe76e7700295ba0b13f02c8b2e7
VolynetsOlha <127936527+VolynetsOlha@users.noreply.github.com>
2023-08-23 18:14:03 +0000

    Deploying to gh-pages from @ VolynetsOlha/goit-js-hw-09@fa4192845d4eff3c9f9bb1f7f78dced75eaac160 🚀

---
+++ 01-color-switcher.bd3492b9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(d)}));let d=null;
+//# sourceMappingURL=01-color-switcher.bd3492b9.js.map
---
+++ 01-color-switcher.f2b155c5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(a)}));var a=null}();
+//# sourceMappingURL=01-color-switcher.f2b155c5.js.map
